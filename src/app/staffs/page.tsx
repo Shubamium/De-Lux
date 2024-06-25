60292b54f89d53c11cea2c779027a5f25d4f88ad
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-06-26 03:23:04 +0700

    Add staff page

---
+++ src/app/staffs/page.tsx
@@ -0,0 +1,273 @@
+import React from "react";
+import "./staff.scss";
+type Props = {};
+
+export default function Staff({}: Props) {
+  return (
+    <main id="page_staff">
+      <section id="staff-heading">
+        <div className="styled-title">
+          <h2>STAFF</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea{" "}
+          </p>
+        </div>
+      </section>
+      <section id="staff-list">
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+        <a href="#" target="_blank" className="staff-card">
+          <div className="staff-art-container">
+            <img
+              src="/graphics/staff_sample.png"
+              alt=""
+              className="staff-art"
+            />
+          </div>
+          <div className="staff-info">
+            <h2>Staff Name</h2>
+            <svg
+              width="199"
+              height="18"
+              viewBox="0 0 199 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.7333 8.86121L9.39355 17.5215L18.0538 8.86121L9.39355 0.200952L0.7333 8.86121ZM198.824 8.86121L190.164 0.200952L181.504 8.86121L190.164 17.5215L198.824 8.86121ZM9.39355 10.3612H190.164V7.36121H9.39355V10.3612Z"
+                fill="#FDDF63"
+              />
+            </svg>
+            <p>Role or title</p>
+          </div>
+        </a>
+      </section>
+    </main>
+  );
+}
