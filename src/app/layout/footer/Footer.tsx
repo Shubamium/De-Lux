5e0ac6d91f79e3269591e971d3b448313a346621
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-06-26 00:56:48 +0700

    Add header decoration

---
+++ src/app/layout/footer/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {};
+
+export default function Footer({}: Props) {
+  return (
+    <footer id="main_footer">
+      <div className="left-side">
+        <p>De♧LUX OFFICIAL WEBSITE</p>
+        <div className="socials">
+          <a href="#"></a>
+        </div>
+      </div>
+      <div className="right-side">
+        <a href="https://twitter.com/Shubamium2">
+          Built and Designed by <b>SHUBAMIUM</b>
+        </a>
+      </div>
+    </footer>
+  );
+}
--- src/app/layout/header/Header.tsx
+++ src/app/layout/header/Header.tsx
@@ -5,42 +5,54 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header id="main_header">
-      <h1 hidden={true}>DeLux Official Website</h1>
-      <img src="./graphics/club.svg" alt="" className="decor_header-clover" />
-
-      <nav className="header-nav">
-        <Link className="btn btn-header-nav selected" href="/">
-          <span>Home</span>
-        </Link>
-        <Link className="btn btn-header-nav" href="/idols">
-          <span>Idols</span>
-        </Link>
-        <Link className="btn btn-header-nav" href="/staffs">
-          <span>Staff</span>
-        </Link>
-      </nav>
-      <div className="logo">
-        <img
-          src="./graphics/main_logo.png"
-          alt="delux-main-logo"
-          className="main_logo"
-        />
-      </div>
-      <nav className="header-nav">
-        <Link className="btn btn-header-nav" href="/idols">
-          <span>DISCOGRAPHY</span>
-        </Link>
-        <Link className="btn btn-header-nav" href="/staffs">
-          <span>CREDITS</span>
-        </Link>
-      </nav>
-
+    <>
       <img
-        src="./graphics/club.svg"
+        src="/decors/card-deck.svg"
         alt=""
-        className="decor_header-clover right"
+        className="decor_card-deck top l"
       />
-    </header>
+      <header id="main_header">
+        <h1 hidden={true}>DeLux Official Website</h1>
+
+        <img src="./graphics/club.png" alt="" className="decor_header-clover" />
+        <nav className="header-nav">
+          <Link className="btn btn-header-nav selected" href="/">
+            <span>Home</span>
+          </Link>
+          <Link className="btn btn-header-nav" href="/idols">
+            <span>Idols</span>
+          </Link>
+          <Link className="btn btn-header-nav" href="/staffs">
+            <span>Staff</span>
+          </Link>
+        </nav>
+        <div className="logo">
+          <img
+            src="./graphics/main_logo.png"
+            alt="delux-main-logo"
+            className="main_logo"
+          />
+        </div>
+        <nav className="header-nav">
+          <Link className="btn btn-header-nav" href="/idols">
+            <span>DISCOGRAPHY</span>
+          </Link>
+          <Link className="btn btn-header-nav" href="/staffs">
+            <span>CREDITS</span>
+          </Link>
+        </nav>
+
+        <img
+          src="./graphics/club.png"
+          alt=""
+          className="decor_header-clover right"
+        />
+      </header>
+      <img
+        src="/decors/card-deck.svg"
+        alt=""
+        className="decor_card-deck top r"
+      />
+    </>
   );
 }
