fe1a6c1a0e243d3609a92bdeaaeb3e1e3417cca1
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-06-26 23:42:47 +0700

    Add home data

--- src/app/layout/header/Header.tsx
+++ src/app/layout/header/Header.tsx
@@ -1,9 +1,20 @@
+"use client";
 import React from "react";
 import "./header.scss";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 type Props = {};
 
 export default function Header({}: Props) {
+  const path = usePathname();
+
+  const isPath = (check: string) => {
+    if (path.includes(check)) {
+      return "selected";
+    } else {
+      return "";
+    }
+  };
   return (
     <>
       <img
@@ -16,13 +27,22 @@ export default function Header({}: Props) {
 
         <img src="./graphics/club.png" alt="" className="decor_header-clover" />
         <nav className="header-nav">
-          <Link className="btn btn-header-nav selected" href="/">
+          <Link
+            className={`btn btn-header-nav ${path === "/" ? "selected" : ""}`}
+            href="/"
+          >
             <span>Home</span>
           </Link>
-          <Link className="btn btn-header-nav" href="/idols">
+          <Link
+            className={`btn btn-header-nav ${isPath("/idols")}`}
+            href="/idols"
+          >
             <span>Idols</span>
           </Link>
-          <Link className="btn btn-header-nav" href="/staffs">
+          <Link
+            className={`btn btn-header-nav ${isPath("/staffs")}`}
+            href="/staffs"
+          >
             <span>Staff</span>
           </Link>
         </nav>
@@ -34,10 +54,16 @@ export default function Header({}: Props) {
           />
         </div>
         <nav className="header-nav">
-          <Link className="btn btn-header-nav" href="/discography">
+          <Link
+            className={`btn btn-header-nav ${isPath("/discography")}`}
+            href="/discography"
+          >
             <span>DISCOGRAPHY</span>
           </Link>
-          <Link className="btn btn-header-nav" href="/credits">
+          <Link
+            className={`btn btn-header-nav ${isPath("/credits")}`}
+            href="/credits"
+          >
             <span>CREDITS</span>
           </Link>
         </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,41 @@
+import { fetchData, urlFor } from "./db/db";
 import "./home.scss";
-export default function Home() {
+export default async function Home() {
+  const homeData = await fetchData<any>(
+    `*[_type == "general" && preset == "main"][0]{
+			...,
+			home_banner{
+				banner_type,
+				'src': src.asset->url
+			}
+		}`
+  );
+  console.log("homeData", homeData.home_banner.src);
   return (
     <main id="page_home">
       <section id="animated-hero">
-        <img src="/graphics/main_banner-placeholder.png" alt="" />
-        {/* <video src=""></video> */}
+        {homeData.home_banner.banner_type == "image" ? (
+          <img src={homeData.home_banner.src} alt="" />
+        ) : (
+          <video
+            src={homeData.home_banner.src}
+            autoPlay={true}
+            loop
+            muted
+            className="video"
+          ></video>
+        )}
       </section>
 
       <section id="about-section">
         <div className="about-panel">
           <img src="/graphics/main_logo.png" alt="" className="about-logo" />
           <img
-            src="/graphics/mini_banner.png"
+            src={urlFor(homeData.about.banner).url()}
             alt=""
             className="about-banner"
           />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <p>{homeData.about.description}</p>
 
           <img src="/graphics/irene.gif" alt="" className="side-idol l " />
           <img src="/graphics/caspy.gif" alt="" className="side-idol r" />
